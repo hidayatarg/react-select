930dc08869714a6353d631562b2d69980fb4e603
Hidayat Arghandabi <hidayatarg@gmail.com>
2021-06-09 00:48:24 +0300

    Implemented Select

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
 
-function App() {
+const Main = styled.div`
+  font-family: sans-serif;
+  background: #f0f0f0;
+  height: 100vh;
+  padding-top:100px;
+`;
+
+const DropDownContainer = styled.div`
+  width: 10.5em;
+  margin: 0 auto;
+`;
+
+const DropDownHeader = styled.div`
+  margin-bottom: 0.8em;
+  padding: 0.4em 2em 0.4em 1em;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+  font-weight: 500;
+  font-size: 1.3rem;
+  color: #3faffa;
+  user-select: none;
+  cursor: pointer;
+`;
+
+const DropDownListContainer = styled.div`
+  position: absolute;
+  z-index: 100;
+  width: 10.5em;
+`;
+
+const DropDownList = styled.ul`
+  padding: 0;
+  margin: 0;
+  padding-left: 1em;
+  background: #ffffff;
+  border: 2px solid #e5e5e5;
+  box-sizing: border-box;
+  color: #3faffa;
+  font-size: 1.3rem;
+  font-weight: 500;
+  &:first-child {
+    padding-top: 0.8em;
+  }
+`;
+
+const ListItem = styled.li`
+  list-style: none;
+  margin-bottom: 0.8em;
+  user-select: none;
+  &:hover {
+    color: #fd9e46;
+  }
+  cursor: pointer;
+`;
+
+const options = [
+  {
+    value: "Mangoes",
+  },
+  {
+    value: "Orange",
+  },
+  {
+    value: "Apple",
+  }
+];
+
+export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const toggling = () => setIsOpen(!isOpen);
+
+  const onOptionClicked = value => () => {
+    setSelectedOption(value);
+    setIsOpen(false);
+    console.log(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+      <DropDownContainer>
+        <DropDownHeader onClick={toggling}>
+          {selectedOption || "Mangoes"}
+        </DropDownHeader>
+        {isOpen && (
+          <DropDownListContainer>
+            <DropDownList>
+              {options.map((option, i) => (
+                <ListItem onClick={onOptionClicked(option.value)} key={i}>
+                  {option.value}
+                </ListItem>
+              ))}
+            </DropDownList>
+          </DropDownListContainer>
+        )}
+      </DropDownContainer>
+    </Main>
   );
 }
-
-export default App;
---
+++ src/style.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from 'styled-components';
+
+const DropDownContainer = styled("div")``;
+const DropDownHeader = styled("div")``;
+const DropDownListContainer = styled("div")``;
+const DropDownList = styled("ul")``;
+const ListItem = styled("li")``;
+
+export default {
+    DropDownContainer,
+    DropDownHeader,
+    DropDownListContainer,
+    DropDownList,
+    ListItem
+}
